633c9f387a8a4c0f832a80d7987ca8c8e3e5de3d
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-10-02 21:11:11 -0400

    Added Nutella#int_input
    This method allows one to nicely take an integer input from the user.

--- lib/nutella/input.rb
+++ lib/nutella/input.rb
@@ -26,5 +26,30 @@ module Nutella
     def input(prompt)
       raw_input(prompt).strip
     end
+
+    # Takes an integer input from the user. If the user does not enter a valid
+    # integer value, display the error and ask them again, unless +err_msg+ is
+    # <tt>nil</tt>, in which case it will not display any error message before
+    # asking again. If +repeat+ is set to false, it will only ask once, exiting
+    # and returning nil if the first attempt fails.
+    #
+    # @example Ask the user to input their age
+    #   age = Nutella::int_input "Enter your age: "
+    #
+    # @param [String] prompt the message to display to the user
+    # @param [String, NilClass] err_msg the error to show the user if they
+    #                                   should enter an invalid input
+    # @param [Boolean] repeat whether or not to continue prompting the user
+    #                         should their first input be unacceptable
+    # @return [Integer] the user's numeric input as an integer
+    def int_input(prompt, err_msg = "Please enter a number.", repeat = true)
+      str = ""
+      until str[/^-?\d+$/]
+        str = input prompt
+        puts err_msg unless str[/^-?\d+$/] || err_msg.nil?
+        return unless repeat
+      end
+      str.to_i
+    end
   end
 end
